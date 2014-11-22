016dccf3394e383f33152cbf53de23a53fe5c484
JasWSInc <jas@wsharks.com>
2014-11-21 15:06:46 -0900

    Completing Twitter SSO. See: #41

--- comment-mail/includes/classes/sso-twitter.php
+++ comment-mail/includes/classes/sso-twitter.php
@@ -21,6 +21,18 @@ namespace comment_mail // Root namespace.
 		class sso_twitter extends sso_service_base
 		{
 			/**
+			 * Class constructor.
+			 *
+			 * @since 141111 First documented version.
+			 *
+			 * @param array $request_args Incoming request args.
+			 */
+			public function __construct(array $request_args)
+			{
+				parent::__construct('twitter', $request_args);
+			}
+
+			/**
 			 * Handle SSO authorization redirection.
 			 *
 			 * @since 141111 First documented version.
@@ -29,22 +41,24 @@ namespace comment_mail // Root namespace.
 			{
 				try // Catch exceptions generated here and log them for debugging.
 				{
+					$redirect_to = $this->request_args['redirect_to'];
+
 					$service_factory = new \OAuth\ServiceFactory();
 					$credentials     = new \OAuth\Common\Consumer\Credentials(
 						$this->plugin->options['sso_'.$this->service.'_key'],
 						$this->plugin->options['sso_'.$this->service.'_secret'],
-						$this->plugin->utils_url->sso_action_url($this->service, 'callback', $this->request_args['redirect_to'])
+						$this->plugin->utils_url->sso_action_url($this->service, 'callback', $redirect_to)
 					);
 					$service         = $service_factory->createService($this->service, $credentials, $this->storage);
 					/** @var $service \OAuth\OAuth1\Service\Twitter */
 
-					if(($token = $service->requestRequestToken()))
+					if(($token = $service->requestRequestToken())) // Must obtain from Twitter.
 						if(($url = $service->getAuthorizationUri(array('oauth_token' => $token->getRequestToken()))))
-							wp_redirect($url).exit();
+							wp_redirect($url).exit(); // Redirect to service and request authorization.
 
-					throw new \exception(__('Failed to generate authorization URL.', $this->plugin->text_domain));
+					throw new \exception(__('Failed to acquire authorization URL.', $this->plugin->text_domain));
 				}
-				catch(\exception $exception)
+				catch(\exception $exception) // Log for debugging.
 				{
 					$this->plugin->utils_log->maybe_debug($exception);
 				}
@@ -76,7 +90,7 @@ namespace comment_mail // Root namespace.
 						$this->request_args['oauth_verifier'],
 						$this->storage->retrieveAccessToken($this->service)->getRequestTokenSecret()
 					);
-					# Acquire and validation data received from this service.
+					# Acquire and validate data received from this service.
 
 					if(!is_object($service_user = json_decode($service->request('account/verify_credentials.json'))))
 						throw new \exception(__('Failed to verify user.', $this->plugin->text_domain));
@@ -90,10 +104,13 @@ namespace comment_mail // Root namespace.
 					if(!($lname = $this->request_args['lname']))
 						$lname = $this->plugin->utils_string->last_name($service_user->name, $this->request_args['email']);
 
-					$email = $this->request_args['email']; // Must come from request args.
+					$email = $this->request_args['email']; // Must come from request args; Twitter sucks.
 
-					if(!$fname || !$email) exit($this->plugin->utils_sso->request_completion());
-
+					if(!$fname || !$email) // Email must come from request args; Twitter will not provide it to us.
+					{
+						$request_completion_args = compact('fname', 'lname', 'email');
+						exit($this->plugin->utils_sso->request_completion($this->request_args, $request_completion_args));
+					}
 					# Process and perform redirection.
 
 					$sso_id                = $service_user->id_str;
@@ -103,7 +120,7 @@ namespace comment_mail // Root namespace.
 					if(!$this->plugin->utils_sso->process_redirect($this->service, $sso_id, $process_redirect_args))
 						throw new \exception(__('Failed to redirect user.', $this->plugin->text_domain));
 				}
-				catch(\exception $exception)
+				catch(\exception $exception) // Log for debugging.
 				{
 					$this->plugin->utils_log->maybe_debug($exception);
 				}
