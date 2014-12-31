533c230d6161e6f1dcef63feccf3c3a185867516
JasWSInc <jas@wsharks.com>
2014-12-30 17:07:25 -0900

    Adding `$_sql` to exception message to improve error reporting.

--- comment-mail/includes/classes/installer.php
+++ comment-mail/includes/classes/installer.php
@@ -57,7 +57,7 @@ namespace comment_mail // Root namespace.
 						$_sql = $this->plugin->utils_db->fulltext_compat($_sql);
 
 						if(!$this->plugin->utils_db->wp->query($_sql)) // Table creation failure?
-							throw new \exception(sprintf(__('DB table creation failure: `%1$s`.', $this->plugin->text_domain), $_sql_file_table));
+							throw new \exception(sprintf(__('DB table creation failure. Table: `%1$s`. SQL: `%2$s`.', $this->plugin->text_domain), $_sql_file_table, $_sql));
 					}
 				unset($_sql_file, $_sql_file_table, $_sql); // Housekeeping.
 			}
