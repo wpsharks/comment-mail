18ece673f637677d9f637c256cb410a78bc3d091
jaswsinc <jas@wsharks.com>
2015-06-26 10:06:26 -0800

    Improve Jetpack conflict warning. See: https://github.com/websharks/comment-mail/issues/113#issuecomment-115808662

--- comment-mail/plugin.inc.php
+++ comment-mail/plugin.inc.php
@@ -1050,8 +1050,8 @@ namespace comment_mail {
 				if(!is_admin() || !empty($_REQUEST['action']))
 					return; // Stay quiet in this case.
 
-				$conflict = sprintf(__('<p style="font-size:120%%; font-weight:400; margin:0;"><strong>%1$s&trade;</strong> + <strong>Jetpack Subscriptions Module</strong> = Possible Conflict!</p>', $this->text_domain), esc_html($this->name));
-				$conflict .= '<p style="margin:0;">'.sprintf(__('<strong>WARNING (ACTION REQUIRED):</strong> Running %1$s&trade; while the Jetpack Subscriptions module is <em>also</em> active in WordPress <strong>can cause problems</strong>; i.e., these two plugins do the same thing—%1$s being the newer of the two. We recommend keeping %1$s; please deactivate the Jetpack Subscriptions module to get rid of this message.', $this->text_domain), esc_html($this->name)).'</p>';
+				$conflict = sprintf(__('<p style="font-size:120%%; font-weight:400; margin:0;"><strong>%1$s&trade;</strong> + <strong>Jetpack Subscriptions module</strong> (with Follow Comments enabled) = Possible Conflict!</p>', $this->text_domain), esc_html($this->name));
+				$conflict .= '<p style="margin:0;">'.sprintf(__('<strong>WARNING (ACTION REQUIRED):</strong> Running %1$s&trade; while the Jetpack Subscriptions module (with Follow Comments enabled) is <em>also</em> active in WordPress <strong>can cause problems</strong>; i.e., these two handle the same thing—%1$s being the newer of the two. We recommend keeping %1$s; please deactivate the Follow Comments functionality in the Jetpack Subscriptions module to get rid of this message.', $this->text_domain), esc_html($this->name)).'</p>';
 				$this->enqueue_error($conflict);
 			}
 
