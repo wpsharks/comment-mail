266542152386c90546baf02064a84c39cd5c7853
Raam Dev <raam@websharks-inc.com>
2015-12-12 17:48:25 -0500

    Remove redundant link to "Your Comment" in email template header
     When the notification is a reply to "Your Comment", the text that follows
     the title is "In response to your comment:", with 'your comment' linked
     to the comment. There is no need to include a second link in the title.
    
    See websharks/comment-mail#77

--- comment-mail/includes/templates/type-s/email/comment-notification/snippet/message-heading.php
+++ comment-mail/includes/templates/type-s/email/comment-notification/snippet/message-heading.php
@@ -4,7 +4,7 @@
 
 		[if sub_comment]
 			[if subscribed_to_own_comment]
-				New Replies to <a href="[sub_comment_url]">your Comment</a> on "[sub_post_title_clip]"
+				New Replies to Your Comment on "[sub_post_title_clip]"
 			[else]
 				New Replies to <a href="[sub_comment_url]">a Comment</a> on "[sub_post_title_clip]"
 			[endif]
@@ -16,7 +16,7 @@
 
 		[if sub_comment]
 			[if subscribed_to_own_comment]
-				New Reply to <a href="[sub_comment_url]">your Comment</a> on "[sub_post_title_clip]"
+				New Reply to Your Comment on "[sub_post_title_clip]"
 			[else]
 				New Reply to <a href="[sub_comment_url]">a Comment</a> on "[sub_post_title_clip]"
 			[endif]
